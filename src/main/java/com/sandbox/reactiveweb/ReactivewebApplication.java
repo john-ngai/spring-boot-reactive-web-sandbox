12acf718b79f7302beaac4ecbc6dbe8905cc0501
mrjohnming <john.ngai@hotmail.com>
2025-02-03 16:04:30 -0500

    Spring Initializr - Spring Boot 3.4.2, Java 17, Maven, Jar
    - Spring Web
    - Spring Reactive Web

---
+++ src/main/java/com/sandbox/reactiveweb/ReactivewebApplication.java
@@ -0,0 +1,13 @@
+package com.sandbox.reactiveweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivewebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandbox/reactiveweb/ReactivewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandbox.reactiveweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
